e567d4fe850ea76cc5dd259efa3399db60e42e97
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-01 12:18:27 -0500

    Update twoSum.py

--- python/twoSum.py
+++ python/twoSum.py
@@ -4,6 +4,7 @@ class Solution(object):
                                # Indicies (an alphabetical list of names, subjects, etc., with references to the places where they occur,)
 
         for i, num in enumerate(nums): # enumerate over list, tracking (i) & (num)
+                                                                                                                                           
             complement = target - num # complement = number of something
 
             if complement in number_dictionary: # if the complement number exsits in the dictionary
@@ -11,6 +12,13 @@ class Solution(object):
             number_dictionary[num] = i # store current num as a key that acts as an index in the dictionary
             
             # [number_dictionary[complement_2], i] = [0, 1].
+            
+            '''
+            i=0, num=2                      complement_1 = 9 - 2 = 7
+            i=1, num=7    ----------->      complement_2 = 9 - 7 = 2         ----------->   Is complement in dictionary?          ----------->   number_dictionary = {
+            i=2, num=11                     complement_3 = 9 - 11 = -2                              Yes? ---> Go it if statement                    2: 0,
+            i=3, num=15                     complement_4 = 9 - 15 = -6                                                                              7: 1              }
+            ''' 
 
 nums = [2, 7, 11, 15]
 target = 9
