9cea57fb2ae40a6e5a3c84a7a8f94bde4c3fb9f3
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-01 08:55:19 -0500

    Update twoSum.cs

--- C#/twoSum.cs
+++ C#/twoSum.cs
@@ -1,1 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) { // two params one is an array
+        Dictionary<int, int> map = new Dictionary<int, int>(); // Dictionary<int, int> to store numbers and their indices
+        // C# does not have a hashmap- instead use dictionary
 
+        for (int i = 0; i < nums.Length; i++) { // loops through nums array and calculates complement for each
+            int complement = target - nums[i];
+            if (map.ContainsKey(complement)) { // ContainsKey(complement): is a method of the Dictionary
+                // map: This is the dictionary (specifically, a Dictionary<int, int>)- to me this is confusing but it is what it is
+                return new int[] { map[complement], i }; // if complement exists- then return
+            }
+            map[nums[i]] = i;
+        }
+
+        throw new ArgumentException("No two numbers add up to the target.");
+    }
+}
