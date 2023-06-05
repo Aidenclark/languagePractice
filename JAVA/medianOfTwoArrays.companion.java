a85ac6e37a208d0ad23454f88e5f5ceb7af049ae
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-05 13:43:08 -0500

    Update medianOfTwoArrays.companion.java

--- JAVA/medianOfTwoArrays.companion.java
+++ JAVA/medianOfTwoArrays.companion.java
@@ -1,1 +1,28 @@
+Certainly! Here's a visualization of the binary search algorithm used in the `findMedianSortedArrays` method:
 
+Let's assume we have two sorted arrays:
+
+`nums1 = [1, 3, 5, 7]`
+`nums2 = [2, 4, 6, 8, 10]`
+
+Initially, the left and right pointers are set as follows:
+`left = 0`
+`right = m` (length of `nums1`)
+
+1. First, we perform the binary search to find the partition points in the `nums1` array. In each iteration, we update the `left` or `right` pointer based on the comparison of `maxLeft1` and `minRight2`.
+
+   - Iteration 1:
+     - `partition1 = (left + right) / 2 = (0 + 4) / 2 = 2`
+     - `partition2 = (m + n + 1) / 2 - partition1 = (4 + 5 + 1) / 2 - 2 = 4 - 2 = 2`
+     - `maxLeft1 = nums1[partition1 - 1] = nums1[2 - 1] = nums1[1] = 3`
+     - `minRight1 = nums1[partition1] = nums1[2] = 5`
+     - `maxLeft2 = nums2[partition2 - 1] = nums2[2 - 1] = nums2[1] = 4`
+     - `minRight2 = nums2[partition2] = nums2[2] = 6`
+     - Since `maxLeft1 <= minRight2` and `maxLeft2 <= minRight1`, we have found the correct partitioning. The median is `(max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2 = (4 + 5) / 2 = 4.5` (since the total number of elements is odd).
+
+   - Iteration 2:
+     - The binary search ends here since the correct partitioning is found, and the median is returned.
+
+In this example, the median of the two sorted arrays is `4.5`.
+
+Note: The visualization assumes that the input arrays are sorted. If the input arrays are not sorted, an exception will be thrown.
