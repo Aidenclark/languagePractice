c2708496f23604a8066c544d42e161f75c252c4f
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-05 10:31:31 -0500

    Update longestSubstring.cs

--- C#/longestSubstring.cs
+++ C#/longestSubstring.cs
@@ -1,1 +1,26 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int maxLength = 0;
+        int left = 0;
+        int right = 0;
+        HashSet<char> set = new HashSet<char>();
+        // HashSet<T>: is a built in C# class where you create a new object to store chars
+        // It has some methods like .Add(), .Remove(), and .Contains()
+        // HashSet<char> implementation ensures that there are no duplicate characters
 
+        while (right < s.Length) {
+            if (!set.Contains(s[right])) {
+                set.Add(s[right]);
+                maxLength = Math.Max(maxLength, set.Count);
+                right++;
+            } else {
+                set.Remove(s[left]);
+                left++;
+            }
+        }
+        // I think it is important to not that the structure of this code is similar to other languages
+        // Obviously the syntax is just different ;)
+
+        return maxLength;
+    }
+}
