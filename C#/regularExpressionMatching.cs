a3ae1d3bfdbeecb30f05a13e8ed45733df60e431
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-08 11:21:25 -0500

    Update regularExpressionMatching.cs

--- C#/regularExpressionMatching.cs
+++ C#/regularExpressionMatching.cs
@@ -1,1 +1,35 @@
-
+public class Solution {
+    public bool IsMatch(string s, string p) {
+        int m = s.Length;
+        int n = p.Length;
+        
+        // Create a 2D DP table
+        bool[,] dp = new bool[m + 1, n + 1];
+        
+        // Base case: an empty pattern matches an empty string
+        dp[0, 0] = true;
+        
+        // Handle the case when s is empty but p has '*'
+        for (int j = 1; j <= n; j++) {
+            if (p[j - 1] == '*') {
+                dp[0, j] = dp[0, j - 2];
+            }
+        }
+        
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
+                    dp[i, j] = dp[i - 1, j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i, j] = dp[i, j - 2];
+                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
+                        dp[i, j] |= dp[i - 1, j];
+                    }
+                }
+            }
+        }
+        
+        return dp[m, n];
+    }
+}
