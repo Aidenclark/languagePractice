f2a5a1b170b8fdd825163b328be9a9ece8ab893a
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-08 11:20:13 -0500

    Update regularExpressionMatching.js

--- JavaScript/regularExpressionMatching.js
+++ JavaScript/regularExpressionMatching.js
@@ -1,1 +1,36 @@
+function isMatch(s, p) {
+  const m = s.length;
+  const n = p.length;
 
+  // Create a 2D DP table
+  const dp = new Array(m + 1);
+  for (let i = 0; i <= m; i++) {
+    dp[i] = new Array(n + 1).fill(false);
+  }
+
+  // Base case: an empty pattern matches an empty string
+  dp[0][0] = true;
+
+  // Handle the case when s is empty but p has '*'
+  for (let j = 1; j <= n; j++) {
+    if (p[j - 1] === '*') {
+      dp[0][j] = dp[0][j - 2];
+    }
+  }
+
+  // Fill the DP table
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (p[j - 1] === s[i - 1] || p[j - 1] === '.') {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else if (p[j - 1] === '*') {
+        dp[i][j] = dp[i][j - 2];
+        if (p[j - 2] === s[i - 1] || p[j - 2] === '.') {
+          dp[i][j] |= dp[i - 1][j];
+        }
+      }
+    }
+  }
+
+  return dp[m][n];
+}
