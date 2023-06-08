7e328fcabccd9f3c29a9f8870bcff905658b5881
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-08 16:20:27 -0500

    Update regularExpressionMatching.cs

--- C#/regularExpressionMatching.cs
+++ C#/regularExpressionMatching.cs
@@ -33,3 +33,33 @@ public class Solution {
         return dp[m, n];
     }
 }
+
+
+We have two loops, one nested inside the other. The outer loop is controlled by the variable i, 
+and it goes from 1 to m (the length of string s). The inner loop is controlled by the variable j, 
+and it goes from 1 to n (the length of pattern p).
+
+Inside these loops, we perform some checks and calculations. Here's what happens:
+
+We check if the character at position j - 1 in the pattern p is equal to the character at 
+position i - 1 in the string s, or if it is a dot ('.'). If it is, it means we have a match, and we set 
+the corresponding value in the dp table to be the same as the value in the dp table for the previous 
+positions i - 1 and j - 1. This is represented by dp[i][j] = dp[i - 1][j - 1].
+
+If the character at position j - 1 in the pattern p is an asterisk ('*'), we have a special case. We set
+the current dp value to be the value two positions before in the same row, which is dp[i][j - 2]. This accounts 
+for the scenario where the asterisk represents zero occurrences of the previous character in the pattern.
+
+Additionally, if the character before the asterisk at position j - 2 in the pattern p is equal to the character 
+at position i - 1 in the string s, or if it is a dot ('.'), we update the current dp value by performing a logical 
+OR operation with the value at the previous row and the same column, dp[i - 1][j]. This accounts for the scenario where 
+the asterisk represents one or more occurrences of the previous character in the pattern.
+
+Finally, the function returns the value in the bottom-right cell of the dp table, which represents whether the entire 
+pattern matches the entire string.
+
+I hope this explanation helps clarify the code for a 5th grader! Let me know if you have any more questions.
+
+
+
+
