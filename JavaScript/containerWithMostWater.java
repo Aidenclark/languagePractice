47fcc31460279a20377117070ecc7621ba3509d0
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-13 13:46:46 -0500

    Update containerWithMostWater.java

--- JavaScript/containerWithMostWater.java
+++ JavaScript/containerWithMostWater.java
@@ -15,5 +15,58 @@ The two-pointer approach is a commonly used technique for solving problems that 
 
 These are just a few examples of the many use cases for the two-pointer approach. It is a versatile technique that can be applied to various scenarios that involve searching, manipulation, or traversal of arrays or linked lists.
 
+
+To solve this problem efficiently, we can use the two-pointer approach. We initialize two pointers, one at the beginning of the array (left) and the other at the end (right). We calculate the area between the lines represented by the pointers and update the maximum area if necessary. Then, we move the pointer with the smaller height towards the center.
+
+
+Let's consider the input height = [1, 8, 6, 2, 5, 4, 8, 3, 7].
+
+We start with two pointers, left and right, at the beginning and end of the array:
+
+
+   |
+[1, 8, 6, 2, 5, 4, 8, 3, 7]
+   |                       |
+ left                   right
+Calculate the area between the lines represented by the pointers:
+
+h = min(height[left], height[right]) = min(1, 7) = 1
+w = right - left = 8 - 0 = 8
+area = h * w = 1 * 8 = 8
+max_area = max(max_area, area) = max(0, 8) = 8
+Since height[left] < height[right], we move the left pointer towards the center:
+
+
+   |
+[1, 8, 6, 2, 5, 4, 8, 3, 7]
+    |                      |
+  left                  right
+Next iteration:
+
+h = min(height[left], height[right]) = min(8, 7) = 7
+w = right - left = 7 - 1 = 6
+area = h * w = 7 * 6 = 42
+max_area = max(max_area, area) = max(8, 42) = 42
+Since height[left] < height[right], we move the left pointer towards the center:
+
+
+    |
+[1, 8, 6, 2, 5, 4, 8, 3, 7]
+     |                     |
+   left                 right
+Next iteration:
+
+h = min(height[left], height[right]) = min(6, 7) = 6
+w = right - left = 6 - 2 = 4
+area = h * w = 6 * 4 = 24
+max_area = max(max_area, area) = max(42, 24) = 42
+Since height[left] < height[right], we move the left pointer towards the center:
+
+
+     |
+[1, 8, 6, 2, 5, 4, 8, 3, 7]
+      |                    |
+    left                right
+
 */ 
 
