9e9583c39ce8eabb7dc50c8cb4901138a75006b4
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-13 13:50:57 -0500

    Update containerWithMostWater.cs

--- C#/containerWithMostWater.cs
+++ C#/containerWithMostWater.cs
@@ -1,1 +1,22 @@
-
+public class Solution {
+    public int MaxArea(int[] height) {
+        int left = 0;
+        int right = height.Length - 1;
+        int maxArea = 0;
+        
+        while (left < right) {
+            int h = Math.Min(height[left], height[right]);
+            int w = right - left;
+            int area = h * w;
+            maxArea = Math.Max(maxArea, area);
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return maxArea;
+    }
+}
