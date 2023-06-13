379b8e8bf1fbdbb3efbcf9a6f0ce5ca7b8693405
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-13 13:49:28 -0500

    Update containerWithMostWater.java

--- JavaScript/containerWithMostWater.java
+++ JavaScript/containerWithMostWater.java
@@ -70,3 +70,25 @@ Since height[left] < height[right], we move the left pointer towards the center:
 
 */ 
 
+
+
+function maxArea(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+    
+    while (left < right) {
+        const h = Math.min(height[left], height[right]);
+        const w = right - left;
+        const area = h * w;
+        maxArea = Math.max(maxArea, area);
+        
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    
+    return maxArea;
+}
