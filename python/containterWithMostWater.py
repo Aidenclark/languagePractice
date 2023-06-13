90949acda7ee23cc3db4bab1eedaa1b7b530e7c4
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-13 13:48:10 -0500

    Update containterWithMostWater.py

--- python/containterWithMostWater.py
+++ python/containterWithMostWater.py
@@ -3,7 +3,7 @@
 class Solution(object):
     def maxArea(self, height):
         left = 0
-        right = len(height) - 1
+        right = len(height) - 1 # we ensure that the right pointer initially points to the last vertical line in the array
         max_area = 0
         
         while left < right:
